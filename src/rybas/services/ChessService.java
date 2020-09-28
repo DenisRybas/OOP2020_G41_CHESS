d3ac3581acea48f0f7a7b23165b9e86f5005eeab
Denis Rybas <denis2002.rybas@gmail.com>
2020-09-28 16:53:27 +0300

    Rook and Queen possible moves added to chess service

--- src/rybas/services/ChessService.java
+++ src/rybas/services/ChessService.java
@@ -50,8 +50,12 @@ public class ChessService {
                 if (field[posX][posY - 1].getFigure() == null) moves.add(field[posX][posY - 1]);
             }
 
-            if (field[posX - 1][posY - 1].getFigure() != null) beatMoves.add(field[posX - 1][posY - 1]);
-            if (field[posX + 1][posY - 1].getFigure() != null) beatMoves.add(field[posX + 1][posY - 1]);
+            if (field[posX - 1][posY - 1].getFigure() != null &&
+                    field[posX - 1][posY - 1].getFigure().getColor() == Figure.Color.BLACK)
+                beatMoves.add(field[posX - 1][posY - 1]);
+            if (field[posX + 1][posY - 1].getFigure() != null &&
+                    field[posX + 1][posY - 1].getFigure().getColor() == Figure.Color.BLACK)
+                beatMoves.add(field[posX + 1][posY - 1]);
         } else {
             if (posY == 1) {
                 for (int i = posY + 1; i < posY + 3; i++) {
@@ -61,8 +65,12 @@ public class ChessService {
                 if (field[posX][posY + 1].getFigure() == null) moves.add(field[posX][posY + 1]);
             }
 
-            if (field[posX - 1][posY + 1].getFigure() != null) beatMoves.add(field[posX - 1][posY + 1]);
-            if (field[posX + 1][posY + 1].getFigure() != null) beatMoves.add(field[posX + 1][posY + 1]);
+            if (field[posX - 1][posY + 1].getFigure() != null &&
+                    field[posX - 1][posY + 1].getFigure().getColor() == Figure.Color.WHITE)
+                beatMoves.add(field[posX - 1][posY + 1]);
+            if (field[posX + 1][posY + 1].getFigure() != null &&
+                    field[posX + 1][posY + 1].getFigure().getColor() == Figure.Color.WHITE)
+                beatMoves.add(field[posX + 1][posY + 1]);
         }
         pm.put(TypeOfMove.MOVE, moves);
         pm.put(TypeOfMove.BEAT, beatMoves);
@@ -70,10 +78,63 @@ public class ChessService {
     }
 
     public static LinkedHashMap<TypeOfMove, LinkedHashSet<Cell>> getRookPossibleMoves(Board board, Cell cell) {
-        return null;
+        LinkedHashMap<TypeOfMove, LinkedHashSet<Cell>> pm = new LinkedHashMap<>();
+        LinkedHashSet<Cell> beatMoves = new LinkedHashSet<>();
+        LinkedHashSet<Cell> moves = new LinkedHashSet<>();
+        Cell[][] field = board.getField();
+        int posX = cell.getCoordinate().x;
+        int posY = cell.getCoordinate().y;
+
+        //TODO: рокировка
+        for (int i = posY; i >= 0; i--) {
+            if (field[posX][i].getFigure() == null) {
+                moves.add(field[posX][i]);
+            } else {
+                if (field[posX][i].getFigure().getColor() != cell.getFigure().getColor())
+                    beatMoves.add(field[posX][i]);
+                break;
+            }
+        }
+
+        for (int i = posY; i < field.length; i++) {
+            if (field[posX][i].getFigure() == null) {
+                moves.add(field[posX][i]);
+            } else {
+                if (field[posX][i].getFigure().getColor() != cell.getFigure().getColor())
+                    beatMoves.add(field[posX][i]);
+                break;
+            }
+        }
+
+        for (int i = posX; i >= 0; i--) {
+            if (field[i][posY].getFigure() == null) {
+                moves.add(field[i][posY]);
+            } else {
+                if (field[i][posY].getFigure().getColor() != cell.getFigure().getColor())
+                    beatMoves.add(field[i][posY]);
+                break;
+            }
+        }
+
+        for (int i = posX; i < field[i].length; i++) {
+            if (field[i][posY].getFigure() == null) {
+                moves.add(field[i][posY]);
+            } else {
+                if (field[i][posY].getFigure().getColor() != cell.getFigure().getColor())
+                    beatMoves.add(field[i][posY]);
+                break;
+            }
+        }
+
+        pm.put(TypeOfMove.MOVE, moves);
+        pm.put(TypeOfMove.BEAT, beatMoves);
+        return pm;
     }
 
     public static LinkedHashMap<TypeOfMove, LinkedHashSet<Cell>> getQueenPossibleMoves(Board board, Cell cell) {
-        return null;
+        return new LinkedHashMap<>() {{
+            putAll(getBishopPossibleMoves(board, cell));
+            putAll(getRookPossibleMoves(board, cell));
+        }};
     }
 }
